20a661e1e6a981bdfeb1fe1508b5e0c9a5dce9ae
hyerim <hyyrim@gmail.com>
2022-01-17 17:09:19 +0900

    add login component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './maker.module.css';
 
-const Maker = (props) => {
+const Maker = ({ authService }) => {
+	const history = useHistory();
+	const onLogout = () => {
+		authService.logout();
+	};
+
+	useEffect(() => {
+		authService.onAuthChange((user) => {
+			if (!user) {
+				history.push('/');
+			}
+		});
+	});
 	return (
 		<section>
-			<Header />
+			<Header onLogout={onLogout} />
 			<h1>Maker</h1>
 			<Footer />
 		</section>
