604971e807e5f28fb9e73997f84421a5878c0477
hyerim <hyyrim@gmail.com>
2022-01-17 22:59:53 +0900

    preview card UI

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,10 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+	const [cards, setCards] = useState([
+		{
+			id: '1',
+			name: 'Hyerim',
+			company: 'home',
+			theme: 'light',
+			title: 'Fronted-Developer',
+			email: 'hyyrim@gmail.com',
+			message: 'go for it',
+			fileName: null,
+			fileURL: null,
+		},
+		{
+			id: '2',
+			name: 'Hyerim',
+			company: 'home',
+			theme: 'dark',
+			title: 'Fronted-Developer',
+			email: 'hyyrim@gmail.com',
+			message: 'go for it',
+			fileName: null,
+			fileURL: null,
+		},
+		{
+			id: '3',
+			name: 'Hyerim',
+			company: 'home',
+			theme: 'colorful',
+			title: 'Fronted-Developer',
+			email: 'hyyrim@gmail.com',
+			message: 'go for it',
+			fileName: null,
+			fileURL: null,
+		},
+	]);
 	const history = useHistory();
 	const onLogout = () => {
 		authService.logout();
@@ -18,9 +55,12 @@ const Maker = ({ authService }) => {
 		});
 	});
 	return (
-		<section>
+		<section className={styles.maker}>
 			<Header onLogout={onLogout} />
-			<h1>Maker</h1>
+			<div className={styles.container}>
+				<Editor cards={cards} />
+				<Preview cards={cards} />
+			</div>
 			<Footer />
 		</section>
 	);
