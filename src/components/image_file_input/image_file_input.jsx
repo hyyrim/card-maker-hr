8914681ef1eede0d56a56b14a29bea6ddb3a8e35
hyerim <hyyrim@gmail.com>
2022-01-18 15:03:42 +0900

    add card-add-form

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './image_file_input.module.css';
 
-const ImageFileInput = (props) => <button>Image</button>;
+const ImageFileInput = (props) => {
+	return (
+		<div>
+			<input type="file" />
+			<button>Image</button>
+		</div>
+	);
+};
 
 export default ImageFileInput;
