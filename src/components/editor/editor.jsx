6c95e285e68d83c1426eb6fe879ed74c804fb38c
hyerim <hyyrim@gmail.com>
2022-01-18 14:31:36 +0900

    add card-edit-form UI

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -5,9 +5,11 @@ import styles from './editor.module.css';
 const Editor = ({ cards }) => (
 	<section className={styles.editor}>
 		<h1 className={styles.title}>Card Editor</h1>
-		{cards.map((card) => (
-			<CardEditForm key={card.id} card={card} />
-		))}
+		<div className={styles.container}>
+			{cards.map((card) => (
+				<CardEditForm key={card.id} card={card} />
+			))}
+		</div>
 	</section>
 );
 
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -5,7 +5,7 @@ import styles from './preview.module.css';
 const Preview = ({ cards }) => (
 	<section className={styles.preview}>
 		<h1 className={styles.title}>Card Preview</h1>
-		<ul>
+		<ul className={styles.cards}>
 			{cards.map((card) => (
 				<Card key={card.id} card={card} />
 			))}
